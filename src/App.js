40c3b3a668095a633e02f023d29511299a6e3740
ddah0329 <aldrlsb03@naver.com>
2024-12-24 00:09:04 +0900

    UI RENEW

--- src/App.js
+++ src/App.js
@@ -28,11 +28,7 @@ const App = () => {
         const gameData = response.data.records.map((record) => ({
           src: record.fields.image[0].url,
           alt: record.fields["게임 명"],
-          // description: record.fields["한줄 설명"] || "설명이 없습니다.",
           videoUrl: record.fields["영상 설명"] || "영상이 없습니다.",
-          // hashtag: record.fields["장르"],
-          // whatGame: record.fields["어떤 게임 좋아해요?"],
-          // numberOfPeople: record.fields["인원수"],
         }));
         setGames(gameData);
       } catch (error) {
@@ -60,9 +56,7 @@ const App = () => {
 
   const stopSpinning = () => {
     if (!isSpinning) return; // Prevent further actions if already stopped
-
     clearInterval(intervalId); // Stop the spinning interval
-
     stopSlotMachine(0); // 차근차근 슬롯 멈추기
   };
 
@@ -86,66 +80,26 @@ const App = () => {
 
   return (
     <div className="container">
-      <div className="black-section">
-        <div className="slot-machine-outter">
-          <div className="slot-machine-inner">
-            <div className="game-course">
-              <div className="game-images">
-                {selectedGames.map((game, index) => (
-                  <div
-                    key={index}
-                    className={`game-slot ${
-                      isSpinning && stoppingIndex < index ? "spinning" : ""
-                    }`}
-                  >
-                    {game && game.src ? (
-                      <img
-                        src={game.src}
-                        alt={game.alt}
-                        style={{
-                          width: "100%",
-                          height: "100%",
-                          objectFit: "cover", // 슬롯 사이즈에 맞추기
-                        }}
-                      />
-                    ) : (
-                      <div className="placeholder">?</div>
-                    )}
-                  </div>
-                ))}
-              </div>
-              <button
-                className="start-button"
-                onClick={isSpinning ? stopSpinning : startSpinning}
-              >
-                {isSpinning ? "STOP" : "START"}
-              </button>
-            </div>
-            {showDetails && ( // Conditionally render game details based on showDetails state
-              <div className="game-details">
-                {selectedGames.map((game, index) => (
-                  <div key={index} className="game-card">
-                    <div className="game-title">
-                      {game ? game.alt : "게임 제목"}
-                    </div>
-                    {game && game.videoUrl !== "영상이 없습니다." && (
-                      <a
-                        href={game.videoUrl}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="video-button"
-                      >
-                        게임 방법
-                      </a>
-                    )}
-                  </div>
-                ))}
-              </div>
-            )}
-          </div>
+      <div className="black-section"></div>
+      <div className="yellow-section"></div>
+      <div className="grid-section"></div>
+      <div className="slot-machine">
+        <div className="circle-section">
+          <div className="circle">1인</div>
+          <div className="circle">2인</div>
+          <div className="circle">3인</div>
+          <div className="circle">4인</div>
+          <div className="circle">5인</div>
+          <div className="circle">6인</div>
+          <div className="circle">7+인</div>
+        </div>
+        <div className="machine-frame"></div>
+        <div className="game-detail">
+          <div className="detail-frame"></div>
+          <div className="detail-frame"></div>
+          <div className="detail-frame"></div>
         </div>
       </div>
-      <div className="grid-section"></div>
     </div>
   );
 };
